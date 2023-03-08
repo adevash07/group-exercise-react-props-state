2ccf2495f7f208e02c1e1a5970eebc7e15288f82
debugger <58630922+adevash07@users.noreply.github.com>
2023-03-08 13:01:58 +0100

    Update: Finish Chuck

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,19 @@
-interface ChuckJokeProps {
+import Joke from "../joke";
 
+interface ChuckJokeProps {
+  jokes: Joke[];
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
-
-
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ jokes }) => {
+  return (
+    <>
+      <div className='joke'>
+        {jokes.map((joke) => {
+          return <p key={joke.id}>{joke.joke}</p>;
+        })}
+      </div>
+    </>
+  );
+};
 
 export default ChuckJoke;
